02fb3964b03dd9e8228c61a6cb78f0984e4f0e16
Шкурат Данил Евгеньевич <shkurat.d@spbec-mining.ru>
2022-03-23 16:56:58 +0300

    Add pow to algebra class

--- TropicalPalm/Algebra.cs
+++ TropicalPalm/Algebra.cs
@@ -6,24 +6,30 @@ using System.Threading.Tasks;
 
 namespace TropicalPalm {
     internal class Algebra {
-        public Algebra(binaryOperation plus, binaryOperation multiply) {
+        public Algebra(binaryOperation plus, binaryOperation multiply, binaryOperation pow) {
             this.plus = plus;
             this.multiply = multiply;
+            this.pow = pow;
         }
 
         public Algebra() {
             this.plus = (double x, double y) => { return x + y; };
             this.multiply = (double x, double y) => { return x * y; };
+            this.pow = (double x, double y) => { return Math.Pow(x, y); };
         }
 
         private binaryOperation plus;
         private binaryOperation multiply;
+        private binaryOperation pow;
 
         public binaryOperation Plus { 
             set { plus = value; }
         }
         public binaryOperation Multiply {
             set { multiply = value; }
+        }
+        public binaryOperation Pow {
+            set { pow = value; }
         }
     }
 }
