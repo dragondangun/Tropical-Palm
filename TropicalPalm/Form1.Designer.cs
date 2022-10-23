87a13caae995cc7bf2757f6b0122787040bbc17e
dragondangun <dan.sura@mail.ru>
2022-10-23 19:05:57 +0300

    Align RMSE labels

--- TropicalPalm/Form1.Designer.cs
+++ TropicalPalm/Form1.Designer.cs
@@ -60,6 +60,7 @@
             this.pathHolderLabel = new System.Windows.Forms.Label();
             this.pathLabel = new System.Windows.Forms.Label();
             this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label8 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.kNumericUpDown)).BeginInit();
             this.toolStrip1.SuspendLayout();
@@ -471,6 +472,17 @@
             this.progressBar.TabIndex = 30;
             this.progressBar.Visible = false;
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(625, 529);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 29);
+            this.label8.TabIndex = 26;
+            this.label8.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -480,6 +492,7 @@
             this.Controls.Add(this.pathLabel);
             this.Controls.Add(this.pathHolderLabel);
             this.Controls.Add(this.manualButton);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.rootMeanSquaredErrorValueLabel);
             this.Controls.Add(this.rootMeanSquaredErrorLabel);
             this.Controls.Add(this.openFileButton);
@@ -554,6 +567,7 @@
         private System.Windows.Forms.Label pathHolderLabel;
         private System.Windows.Forms.Label pathLabel;
         private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label8;
     }
 }
 
