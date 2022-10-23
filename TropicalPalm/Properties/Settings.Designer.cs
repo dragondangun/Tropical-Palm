fd9c9fbe592e25318065e13101c46fcc09f5207b
dragondangun <dan.sura@mail.ru>
2022-10-23 19:07:24 +0300

    v0.9.1

--- TropicalPalm/Properties/Settings.Designer.cs
+++ TropicalPalm/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace TropicalPalm.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0.9")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.9.1")]
         public string version {
             get {
                 return ((string)(this["version"]));
